289bf8bc5b1aa8f4fed1c37bedbc060ad1e4cf8e
yuvaraj <yuvarajreddy24@gmail.com>
2025-10-14 23:58:22 +0530

    .

---
+++ count/variables.tf
@@ -0,0 +1,11 @@
+variable "instances" {
+    default = [ "mongodb", "redis", "mysql", "rabbitmq" ]
+}
+
+variable "zone_id" {
+    default = "Z00777701H4XIKRG6FJNQ"
+}
+
+variable "domain_name" {
+    default = "yuvarajreddy.fun"
+}
