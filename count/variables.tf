45b84c70cd61c16e08f561a43ebef15f5d753039
yuvaraj <yuvarajreddy24@gmail.com>
2025-10-16 18:32:05 +0530

    .

--- count/variables.tf
+++ count/variables.tf
@@ -1,6 +1,6 @@
 variable "instances" {
     default = [ "mongodb", "redis", "mysql", "rabbitmq", "catalogue", "user", "cart", "shipping", "payment", "frontend" ]
-
+}
 variable "zone_id" {
     default = "Z00777701H4XIKRG6FJNQ"
 }
